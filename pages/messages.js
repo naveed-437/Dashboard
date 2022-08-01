b45dba4d49c5a5be541cf526cda8572d191ba579
Mohammed Naveed <naveed@bassure.com>
2022-08-01 12:43:57 +0530

    commit

--- pages/messages.js
+++ pages/messages.js
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 const Messages = () => {
   return (
     <div class="bg-image d-flex justify-content-center align-items-center aab">
-      <h1 class="text-warning">WELCOME TO Dappr</h1>
+      <h1 class="text-warning">WELCOME TO Dappr Portal</h1>
     </div>
   );
 };
